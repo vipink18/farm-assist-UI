a5bb74e7586490c500ec6f5ccc5489eb6bc1026f
vipink18 <65859121+vipink18@users.noreply.github.com>
2022-10-11 00:11:19 +0530

    Update Button.jsx

--- farm-assist/src/components/Button.jsx
+++ farm-assist/src/components/Button.jsx
@@ -3,7 +3,7 @@ import './button.css'
 
 const Button = (props) => {
   return (
-    <div className='btn'><a to={props.href} className='alink'><span>{props.value}</span></a></div>
+    <div className='btn'><a href={props.href} className='alink'><span>{props.value}</span></a></div>
   )
 }
 
